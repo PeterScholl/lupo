daec7fe0721cacc6c8100a43327d39443231679d
Peter Scholl <scholl@unterrichtsportal.org>
2024-01-27 21:14:18 +0100

    Faecher und Script in index.html entfernt - Funktion gibStundenzahl ergänzt

--- javascript/c_Fachbelegung.js
+++ javascript/c_Fachbelegung.js
@@ -4,13 +4,13 @@ diese Belegung kann auch leer sein...
 class Fachbelegung {
     bezeichnung = "Deutsch";
     kuerzel = "D"
-    belegung = ['','','','','','']; //nicht belegt
+    belegung = ['', '', '', '', '', '']; //nicht belegt
     belegungsBed = new BelegBed(); // Stundenzahl, M,S,LK,ZK möglich
-    bgcolor="#DF0101";
+    bgcolor = "#DF0101";
 
     constructor(bezeichnung, kuerzel) {
-        this.bezeichnung=bezeichnung;
-        this.kuerzel=kuerzel;
+        this.bezeichnung = bezeichnung;
+        this.kuerzel = kuerzel;
     }
 
     /**
@@ -20,15 +20,31 @@ class Fachbelegung {
      * @param {*} halbjahr 0-5 in dem die Belegung hochgesetzt werden soll
      */
     setzeBelegungWeiter(halbjahr) {
-        const bel_neu = this.belegungsBed.gibNaechsteBelegungsmöglichkeit(halbjahr,this.belegung[halbjahr]);
+        const bel_neu = this.belegungsBed.gibNaechsteBelegungsmöglichkeit(halbjahr, this.belegung[halbjahr]);
         this.belegung[halbjahr] = bel_neu;
-        //TODO Bei Q1 auch die Folgebelegungen entsprechend setzen
-        if (halbjahr>1) { //in der Q1
-            let folgeHalbjahr = halbjahr+1;
-            while (folgeHalbjahr<6 && this.belegungsBed.istGueltig(folgeHalbjahr,bel_neu)) { //gültig
-                this.belegung[folgeHalbjahr]=bel_neu;
+        // Bei Q1 auch die Folgebelegungen entsprechend setzen
+        if (halbjahr > 1) { //in der Q1
+            let folgeHalbjahr = halbjahr + 1;
+            while (folgeHalbjahr < 6 && this.belegungsBed.istGueltig(folgeHalbjahr, bel_neu)) { //gültig
+                this.belegung[folgeHalbjahr] = bel_neu;
                 folgeHalbjahr++;
             }
         }
+    }
+
+    /**
+     * gibt die zu wertende Stundenzahl im angebgebenen Halbjahr (0-5)
+     * @param {*} halbjahr 0-5
+     * @returns Stundenzahl für dieses Halbjahr
+     */
+    gibStundenzahlImHalbjahr(halbjahr) {
+        if (halbjahr >= 0 && halbjahr < 6 && this.belegung[halbjahr] != '') {
+            if (this.belegung[halbjahr] == 'LK') {
+                return 5;
+            } else {
+                return this.belegungsBed.stundenzahlen[halbjahr];
+            }
+        }
+        return 0;
     }
 }
