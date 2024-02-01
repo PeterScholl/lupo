598e5fb5a4fe9b08ff0d291aef5279a20c5aca6a
damian5151 <online@mixbox.pl>
2024-02-01 10:16:29 +0100

    Hilfsfunktionen und scrolling hinzugefügt

---
+++ javascript/hilfsfunktionen.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("Belegungsverpflichtung").style.overflow = "scroll";
+    document.getElementById("Klausurverpflichtung").style.overflow = "scroll";
+    document.getElementById("Informationen").style.overflow = "scroll";
+}
